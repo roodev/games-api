f5b1518876596edeaddf2ff218c4548218b9d498
roodev <rodrigo.fessore@gmail.com>
2020-08-23 00:34:42 -0300

    Disponibilizando a rota visualizar um para visualizar um game

--- src/app/routes/games.routes.js
+++ src/app/routes/games.routes.js
@@ -4,5 +4,6 @@ const Game= require('./../controllers/games.controller')
 
 route.post('/criar', Game.criarGame)
 route.get('/visualizarTodos', Game.visualizarGames)
+route.get('/visualizarUm/:nome', Game.visualizarUmGame)
 
 module.exports= route
