8f117d4ef96f42150cca73bacbc468c58fde50ad
roodev <rodrigo.fessore@gmail.com>
2020-08-22 21:52:31 -0300

    Disponibilizando rota para criar game

---
+++ src/app/routes/games.routes.js
@@ -0,0 +1,7 @@
+const express= require('express')
+const route= express.Router()
+const Game= require('./../controllers/games.controller')
+
+route.post('criar', Game.criarGame)
+
+module.exports= route
