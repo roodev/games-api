2c75309ef61282524891652c95125b32005b82d2
roodev <rodrigo.fessore@gmail.com>
2020-08-22 22:00:17 -0300

    Corrigindo a ausência de "/" em route.post

--- src/app/routes/games.routes.js
+++ src/app/routes/games.routes.js
@@ -2,6 +2,6 @@ const express= require('express')
 const route= express.Router()
 const Game= require('./../controllers/games.controller')
 
-route.post('criar', Game.criarGame)
+route.post('/criar', Game.criarGame)
 
 module.exports= route
