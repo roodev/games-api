8b48979b10b92d14a7b8ec6ece5d9b13d1b85e95
roodev <rodrigo.fessore@gmail.com>
2020-09-29 00:11:00 -0300

    removendo rota visualizarUma em decorrencia da implementacao da listarUma

--- src/app/routes/developers.routes.js
+++ src/app/routes/developers.routes.js
@@ -2,10 +2,9 @@ const express= require('express')
 const route= express.Router()
 const Developer= require('./../controllers/developers.controller')
 
-route.post('/listarTodas', Developer.buscarTodasAsDevelopers)
-route.get('/listarUma/: nomeDeveloper', Developer.bucarUmaDeveloperPeloNome)
+route.get('/listarTodas', Developer.buscarTodasAsDevelopers)
+route.get('/listarUma/:nomeDeveloper', Developer.bucarUmaDeveloperPeloNome)
 route.post('/criar', Developer.criarUmaDeveloper)
-route.get('/visualizarUma/:nome', Developer.visualizarUmaDeveloper)
 route.put('/atualizarUma/:nome', Developer.atualizarUmaDeveloper)
 route.delete('/apagarUma/:nome', Developer.apagarUmaDeveloper)
 
